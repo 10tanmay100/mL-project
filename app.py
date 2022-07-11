1bc9b913195e1434a9c5c940dd0e1dd5e9d81267
Tanmay Chakraborty <chakrabortytanmay326@gmail.com>
2022-07-11 22:13:29 +0530

    exception init addition done

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import housing_exception
 
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 
 def home():
+    try:
+        raise Exception("error testing")
+    except Exception as e:
+        housing_exception(e,sys)
+        logging.info(f"error {e} happened!!")
+        logging.info(msg="Inside the home tab")
     return "CI CD Pipeline has been created"
 
 if __name__=="__main__":
