c679d760b9275653aed4983da465909e2c9674f5
Tanmay Chakraborty <chakrabortytanmay326@gmail.com>
2022-07-11 20:25:09 +0530

    setup py has been changed

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
+from gettext import install
 from setuptools import setup
 from typing import List
 
 
-# def get_requirements_list()->List[str]:
-#     '''
-#     This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
-#     '''
-#     with open("requirements.txt","r") as f:
-#         return [line.replace('\n', '') for line in f.readlines()]
+def get_requirements_list()->List[str]:
+    '''
+    This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
+    '''
+    with open("requirements.txt","r") as f:
+        return f.readlines().remove("-e .")
 
 setup(
 name="housing-predictor",
 version="0.0.3",
 author="Tanmay",
 description="This is house price prdiction setup file",
-packages=["housing"]
+packages=["housing"],
+install_requires=get_requirements_list()
 )
 
 
