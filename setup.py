e37d2215faceb4834aeae96469c39584d060965f
Tanmay Chakraborty <chakrabortytanmay326@gmail.com>
2022-07-11 20:10:43 +0530

    new env created

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup
 from typing import List
 
 
-def get_requirements_list()->List[str]:
-    '''
-    This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
-    '''
-    with open("requirements.txt","r") as f:
-        return f.readlines()
+# def get_requirements_list()->List[str]:
+#     '''
+#     This function will going to return a list of strings which will contain name for all the libraries specified with requirements.txt
+#     '''
+#     with open("requirements.txt","r") as f:
+#         return [line.replace('\n', '') for line in f.readlines()]
 
 setup(
 name="housing-predictor",
-version="0.0.1",
+version="0.0.3",
 author="Tanmay",
 description="This is house price prdiction setup file",
-packages=["housing"],
-install_requires=get_requirements_list()
+packages=["housing"]
 )
 
 
