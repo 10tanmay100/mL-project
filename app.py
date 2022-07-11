92c75daed219d339d0683f4a0f4d3a7268f3e3f3
Tanmay Chakraborty <chakrabortytanmay326@gmail.com>
2022-07-11 13:32:48 +0530

    ci cd text added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 
 def home():
-    return "Starting mL project"
+    return "CI CD Pipeline is being created"
 
 if __name__=="__main__":
     app.run(debug=True)
